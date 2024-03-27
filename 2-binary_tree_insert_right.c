2791d36b3513ca8ac9f6c5bd0623e607741a8817
NecheHephzibah <neche.bless@gmail.com>
2024-03-27 02:38:00 -0700

    Is leaf

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+
+/**
+  * binary_tree_insert_left - function that inserts a node to the left
+  * of another node.
+  * @parent: The pointer to the parent node.
+  * @value: The value to store in the new node.
+  * Return: a pointer to the new node, or NULL on failure or if
+  * the parent os NULL.
+  */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	if (parent == NULL)
+		return (NULL);
+
+	newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+    	newnode->right = parent->right;
+
+		parent->right->parent = newnode;
+
+	}
+	parent->right = newnode;
+	return (newnode);
+}
